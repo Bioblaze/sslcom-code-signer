7d15f1535a8aa302a51f9726dce5975cb1440efa
Randolph W. Aarseth II <bioblazepayne@gmail.com>
2024-03-07 11:49:05 -0700

    Added the ability to create the folder, if it does not exist for output, as well as only do supported filetypes when signing files in the batch type process

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,9 @@ fi
 
 BASE_COMMAND="/usr/bin/codesign"
 
+# Supported file types
+SUPPORTED_FILE_TYPES="acm ax bin cab cpl dll drv efi exe mui ocx scr sys tsp msi ps1 ps1xml js vbs wsf jar"
+
 # Common flags for all executions
 COMMON_FLAGS=""
 [ ! -z $INPUT_COMMAND ] && COMMON_FLAGS="$COMMON_FLAGS $INPUT_COMMAND"
@@ -27,33 +30,51 @@ COMMON_FLAGS=""
 [ ! -z $INPUT_MALWARE_BLOCK ] && [ "$INPUT_COMMAND" != "batch_sign" ] && COMMON_FLAGS="${COMMON_FLAGS} -malware_block=${INPUT_MALWARE_BLOCK}"
 [ ! -z $INPUT_OVERRIDE ] && [ "$INPUT_COMMAND" != "batch_sign" ] && COMMON_FLAGS="${COMMON_FLAGS} -override=${INPUT_OVERRIDE}"
 
+# Check and create OUTPUT_PATH if not exists
+if [ ! -d "$INPUT_OUTPUT_PATH" ]; then
+    mkdir -p "$INPUT_OUTPUT_PATH"
+fi
+
 # Handling individual file signing if a directory is specified
 if [ ! -z $INPUT_DIR_PATH ] && [ -d "$INPUT_DIR_PATH" ]; then
     for file in "$INPUT_DIR_PATH"/*; do
-        FILE_COMMAND="$BASE_COMMAND $COMMON_FLAGS -input_file_path \"$file\""
-        echo "Processing file: $file"
-        echo $FILE_COMMAND
-        FILE_RESULT=$(bash -c "set -e; $FILE_COMMAND 2>&1")
-        if [[ "$FILE_RESULT" =~ .*"Error".* || "$FILE_RESULT" =~ .*"Exception".* ]]; then
-            echo "::error::Something Went Wrong with file $file. Please try again."
-            echo "::error::$FILE_RESULT"
-            exit 1
+        # Extract file extension
+        file_ext="${file##*.}"
+        
+        # Check if file extension is in the list of supported file types
+        if [[ $SUPPORTED_FILE_TYPES =~ (^|[[:space:]])$file_ext($|[[:space:]]) ]]; then
+            FILE_COMMAND="$BASE_COMMAND $COMMON_FLAGS -input_file_path \"$file\""
+            echo "Processing file: $file"
+            echo $FILE_COMMAND
+            FILE_RESULT=$(bash -c "set -e; $FILE_COMMAND 2>&1")
+            if [[ "$FILE_RESULT" =~ .*"Error".* || "$FILE_RESULT" =~ .*"Exception".* ]]; then
+                echo "::error::Something Went Wrong with file $file. Please try again."
+                echo "::error::$FILE_RESULT"
+                exit 1
+            else
+                echo "$FILE_RESULT"
+            fi
         else
-            echo "$FILE_RESULT"
+            echo "Skipping unsupported file type: $file"
         fi
     done
 elif [ ! -z $INPUT_FILE_PATH ]; then
-    # Single file processing
-    FINAL_COMMAND="$BASE_COMMAND $COMMON_FLAGS -input_file_path \"$INPUT_FILE_PATH\""
-    echo $FINAL_COMMAND
-    RESULT=$(bash -c "set -e; $FINAL_COMMAND 2>&1")
-    if [[ "$RESULT" =~ .*"Error".* || "$RESULT" =~ .*"Exception".* ]]; then
-        echo "::error::Something Went Wrong. Please try again."
-        echo "::error::$RESULT"
-        exit 1
+    # Check if file extension is in the list of supported file types for a single file
+    file_ext="${INPUT_FILE_PATH##*.}"
+    if [[ $SUPPORTED_FILE_TYPES =~ (^|[[:space:]])$file_ext($|[[:space:]]) ]]; then
+        FINAL_COMMAND="$BASE_COMMAND $COMMON_FLAGS -input_file_path \"$INPUT_FILE_PATH\""
+        echo $FINAL_COMMAND
+        RESULT=$(bash -c "set -e; $FINAL_COMMAND 2>&1")
+        if [[ "$RESULT" =~ .*"Error".* || "$RESULT" =~ .*"Exception".* ]]; then
+            echo "::error::Something Went Wrong. Please try again."
+            echo "::error::$RESULT"
+            exit 1
+        else
+            echo "SELECTED_COLOR=green" >> $GITHUB_OUTPUT
+            echo "$RESULT"
+        fi
     else
-        echo "SELECTED_COLOR=green" >> $GITHUB_OUTPUT
-        echo "$RESULT"
+        echo "Skipping unsupported file type: $INPUT_FILE_PATH"
     fi
 fi
 
